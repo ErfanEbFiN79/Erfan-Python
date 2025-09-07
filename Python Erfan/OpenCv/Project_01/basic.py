c6302af0ff2312647ee8c9f934b205beea77050a
Mohammad Erfan Najafi <ebfin.work@gmail.com>
2025-09-07 16:54:01 +0200

    Full basic function
    Converting to grayscale
    Blur image
    Edge Cascade
    Dilating the image
    Eroding
    Resize
    Cropping

--- Python Erfan/OpenCv/Project_01/basic.py
+++ Python Erfan/OpenCv/Project_01/basic.py
@@ -7,4 +7,31 @@ cv.imshow('Cat 1', img)
 gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 cv.imshow('Gray cat', gray)
 
+# blur image
+Blur = cv.GaussianBlur(img, (3,3), cv.BORDER_DEFAULT)
+cv.imshow("Blur cat", Blur)
+Blur2 = cv.GaussianBlur(img, (7,7), cv.BORDER_DEFAULT)
+cv.imshow("Blur cat2", Blur2)
+
+# Edge Cascade
+canny = cv.Canny(img, 125,175)
+cv.imshow('Cat Canny', canny)
+
+# Dilating the image
+canny = cv.Canny(Blur2, 125,175)
+dilated = cv.dilate(canny, (10,10), iterations=5)
+cv.imshow('Cat Dilated', dilated)
+
+# Eroding
+eroded = cv.erode(dilated, (3,3), iterations=1)
+cv.imshow('Cat Eroded', eroded)
+
+# Resize
+resized1 = cv.resize(img, (40,40))
+cv.imshow('Cat resized1', resized1)
+
+# Cropping
+cropped = img[20: 100, 100:200]
+cv.imshow('Cat Cropped', cropped)
+
 cv.waitKey(0)
