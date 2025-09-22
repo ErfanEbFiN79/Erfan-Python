14a0cae5c73e9f52cafa4ea2e257fd93b6c13b52
Mohammad Erfan Najafi <ebfin.work@gmail.com>
2025-09-22 18:40:03 +0200

    OpenCv
    Work with transformation

--- Python Erfan/OpenCv/Project_01/Transformmation.py
+++ Python Erfan/OpenCv/Project_01/Transformmation.py
@@ -18,4 +18,34 @@ def translate(img,x,y):
 translated = translate(img, 100, 100)
 cv.imshow('New York translated1', translated)
 
+#Rotation
+def rotate(img, angle, rotationPoint=None):
+    (hight,width) = img.shape[:2]
+
+    if rotationPoint is None:
+        rotationPoint = (width//2, hight//2)
+    
+    rotMat = cv.getRotationMatrix2D(rotationPoint,angle,1.0)
+    dimentions = (width,hight)
+
+    return cv.warpAffine(img,rotMat, dimentions)
+
+rotated = rotate(img,45)
+cv.imshow('New York rotate_01', rotated)
+
+#Resizing
+
+resized = cv.resize(img,(500,500),interpolation=cv.INTER_CUBIC)
+cv.imshow('New York resized_01', resized)
+
+#Flipping
+flip = cv.flip(img,flipCode=0)
+# You can use 0,1 and -1 for flip code
+cv.imshow('New York flip', flip)
+
+#Cropping
+cropped = img[200:400, 300:400]
+cv.imshow('New York cropped', cropped)
+
+
 cv.waitKey(0)
